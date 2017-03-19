d3dcf058e0d24977736778fd3580e60279698071
o355 <owen@owenthe.ninja>
2017-03-19 12:37:33 -0400

    Totally coming along great

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import pygame
-import appJar
+from appJar import gui
 
 def musicButtons(btnName):
-    
+    if btnName == "15 minutes of Jeopardy think music":
+        pygame.mixer.Sound(file="15mins_jeopardy.mp3")
+        pygame.mixer.Sound.play()
+    elif btnName == "1 hour of Jeopardy think music":
+        pygame.mixer.Sound(file="1hr_jeopardy.mp3")
+        pygame.mixer.Sound.play()
+    elif btnName == "1 hour of elevator music":
+        pygame.mixer.Sound(file="1hr_elevator.mp3")
+        pygame.mixer.Sound.play()
+    elif btnName == "Stop Music":
+        pygame.mixer.Sound.stop()
+
+app = gui()
+pygame.mixer.init()
+app.addButton("15 minutes of Jeopardy think music")
+app.addButton("1 hour of Jeopardy think music")
+app.addButton("1 hour of elevator music")
+app.addButton("Stop Music")
+
+app.go()
